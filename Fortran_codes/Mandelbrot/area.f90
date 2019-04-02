ae5de8e4760cbca7e95ed7fcfb82ca42a51394d3
tylerjackoliver <tyler.jackoliver@gmail.com>
2019-04-02 10:41:10 +0100

    Adds Mandelbrot solution

--- Fortran_codes/Mandelbrot/area.f90
+++ Fortran_codes/Mandelbrot/area.f90
@@ -1,4 +1,7 @@
 program area_mandelbrot
+
+  use omp_lib
+
   implicit none
 !
   integer, parameter :: sp = kind(1.0)   
@@ -7,7 +10,9 @@ program area_mandelbrot
   integer, parameter :: npoints = 2000, maxiter = 2000
   real (kind=dp) :: area, error
   complex (kind=dp) :: c , z
+  integer :: nthreads = 2
 
+  real :: start, finish ! Timing variables
 !
 ! Calculate area of mandelbrot set
 !
@@ -16,9 +21,21 @@ program area_mandelbrot
 !    Inner loop has the iteration z=z*z+c, and threshold test
 !
 
+  call OMP_SET_NUM_THREADS(nthreads)
+
+  ! Test we have a parallel region
+  !$OMP PARALLEL DO
+  do i=1,nthreads
+
+    write(*,*) "Hello from thread", OMP_GET_THREAD_NUM()
+
+  end do
+  !$OMP END PARALLEL DO
+
+  start = OMP_GET_WTIME()
+
   numoutside = 0 
-
-
+  !$OMP PARALLEL DO PRIVATE(J, C, Z, ITER), REDUCTION(+:NUMOUTSIDE)
   do i = 0,npoints-1 
      do j= 0,npoints-1 
         c = cmplx(-2.0+(2.5*i)/npoints + 1.0d-07,(1.125*j)/npoints + 1.0d-07)
@@ -34,6 +51,7 @@ program area_mandelbrot
         end do 
      end do
   end do
+  !$OMP END PARALLEL DO
 
 
 !
@@ -43,5 +61,10 @@ program area_mandelbrot
   error = area/real(npoints)
   print *, "Area of Mandelbrot set = ",area," +/- ",error
 !
+
+  finish = OMP_GET_WTIME()
+
+  write(*,*) "Time required:", finish-start
+
   stop
 end program area_mandelbrot
