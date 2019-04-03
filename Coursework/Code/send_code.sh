f1cb81a56bac1e397babc16c50001a516688ca31
tylerjackoliver <tyler.jackoliver@gmail.com>
2019-04-03 16:27:40 +0100

    Fixes typo in send_code.sh

--- Coursework/Code/send_code.sh
+++ Coursework/Code/send_code.sh
@@ -2,4 +2,4 @@
 
 # Script to send the code from the local machine to the ARCHER system
 
-rsync -azP *.f90 /jtyler@login.archer.ac.uk:/work/y14/y14/jtyler/coursework/
+rsync -azP *.f90 jtyler@login.archer.ac.uk:/work/y14/y14/jtyler/coursework/
